4f39e3a7f3b0fa081fcf1d71599180cd15d832ee
Dave Johnson <dave.al.johnson@gmail.com>
2020-08-02 21:36:42 -0400

    Initial Commit

---
+++ SwiftUIExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExamples
+//
+//  Created by Dave Johnson on 8/2/20.
+//  Copyright © 2020 Dave Johnson Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
